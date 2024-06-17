20a92e8c0f4db0e9ba9e3d00e6eb7ae75b4617a1
ScottCPetty <scottishcheese13@gmail.com>
2024-06-17 12:14:18 -0400

    Finished SingUpForm

--- src/components/SignUpForm.jsx
+++ src/components/SignUpForm.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useState } from "react";
 
 export default function SignUpForm() {
@@ -6,12 +5,28 @@ export default function SignUpForm() {
   const [password, setPassword] = useState("");
   const [error, setError] = useState(null);
   async function handleSubmit(event) {
-    event.preventDefault()
-    console.log("Hello")
+    event.preventDefault();
+    try {
+      const response = await fetch(
+        "https://fsa-jwt-practice.herokuapp.com/signup",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ username, password }),
+        }
+      );
+      const result = await response.json();
+      console.log(result);
+    } catch (error) {
+      setError(error.message);
+    }
   }
   return (
     <>
       <h2>Sign Up</h2>
+      {error && <p>{error}</p>}
       <form onSubmit={handleSubmit}>
         <label>
           Username:{" "}
