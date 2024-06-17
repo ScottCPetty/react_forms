ea58977b828b9e4e921a8ba5198003a8b89da77e
ScottCPetty <scottishcheese13@gmail.com>
2024-06-17 11:58:45 -0400

    Form functionality sorted

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { useState } from "react";
 import "./App.css";
+import SignUpForm from "./components/SignUpForm";
+import Authenticate from "./components/Authenticate";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <>
-  
-  </>;
+  return (
+    <>
+      <SignUpForm />
+      <Authenticate />
+    </>
+  );
 }
 
 export default App;
