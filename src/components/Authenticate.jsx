25152c46225ac51ce4994093c7e778906bd447d1
ScottCPetty <scottishcheese13@gmail.com>
2024-06-17 12:35:45 -0400

    Created handleClick for Authenticate

--- src/components/Authenticate.jsx
+++ src/components/Authenticate.jsx
@@ -1,9 +1,20 @@
-import React from "react";
+import { useState } from "react";
 
-export default function Authenticate() {
+export default function Authenticate({ token }) {
+    const [error, setError] = useState(null);
+    async function handleClick() {
+        try {
+            //
+        } catch (error) {
+            setError(error.message)
+        }
+        console.log("handleClick ran")
+    }
   return (
     <>
       <h2>Authenticate</h2>
+      {error && <p>{error}</p>}
+      <button onClick={handleClick}>Authenticate Token</button>
     </>
   );
 }
